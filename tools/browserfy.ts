a95297672adbc5dfd4fe1c9d4193229bebb801a8
Weslley Araújo <46850407+wellwelwel@users.noreply.github.com>
2024-09-19 22:52:58 -0300

    fix: improve browser compatibility (#44)
    * fix: improve browser compatibility
    
    * docs: improve description

--- tools/browserfy.ts
+++ tools/browserfy.ts
@@ -1,20 +1,50 @@
-import { readFile } from 'node:fs/promises';
+import { readFile, writeFile, mkdir } from 'node:fs/promises';
 import esbuild from 'esbuild';
+import { transformAsync } from '@babel/core';
+import { minify } from 'terser';
 
 (async () => {
-  const contents = (await readFile('src/index.ts', 'utf8'))
-    .replace(/export/gim, '')
-    .replace(/(let|const) /gim, 'var ');
+  const contents = (await readFile('src/index.ts', 'utf8')).replace(
+    /export const /gim,
+    'window.'
+  );
 
-  await esbuild.build({
+  const result = await esbuild.build({
     stdin: {
       contents,
       loader: 'ts',
     },
     platform: 'browser',
-    target: 'es6',
-    bundle: false,
     outfile: 'browser/lru.min.js',
+    target: 'es6',
     minify: true,
+    write: false,
   });
+
+  const browserfy = await transformAsync(result.outputFiles[0].text, {
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          exclude: ['transform-regenerator'],
+          loose: true,
+          bugfixes: true,
+        },
+      ],
+    ],
+    compact: true,
+    minified: true,
+    comments: false,
+  });
+
+  const minified = await minify(browserfy?.code!, {
+    compress: true,
+    mangle: true,
+    output: {
+      comments: false,
+    },
+  });
+
+  await mkdir('browser');
+  await writeFile('browser/lru.min.js', minified.code!, 'utf8');
 })();
